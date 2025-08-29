244cf219bd40f487ad48c3f3ee2fa0d71cdd835d
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-08-29 18:18:17 +0500

    commited

---
+++ app.js
@@ -0,0 +1,86 @@
+// Chapter 26 to 30
+
+// Question 1
+
+// let positiveNumber = Number(prompt("Enter a positive integer with decimal."))
+
+// let posNumRound = Math.round(positiveNumber)
+// let posNumFloor = Math.floor(positiveNumber)
+// let posNumCeil = Math.ceil(positiveNumber)
+
+// document.writeln("Number:" + " " + positiveNumber + "<br>")
+// document.writeln("Round off value:" + " " + posNumRound + "<br>")
+// document.writeln("Floor value:" + " " + posNumFloor + "<br>")
+// document.writeln("Ceil value:" + " " + posNumCeil + "<br>")
+
+// Question 2
+
+// let negativeNumber = Number(prompt("Enter a negative floating value with decimal."))
+
+// let negNumRound = Math.round(negativeNumber)
+// let negNumFloor = Math.floor(negativeNumber)
+// let negNumCeil = Math.ceil(negativeNumber)
+
+// document.writeln("Number:" + " " + negativeNumber + "<br>")
+// document.writeln("Round off value:" + " " + negNumRound + "<br>")
+// document.writeln("Floor value:" + " " + negNumFloor + "<br>")
+// document.writeln("Ceil value:" + " " + negNumCeil + "<br>")
+
+// Question 3
+
+// var value = Number(prompt("Enter a integer."))
+// var absValue = Math.abs(value)
+
+// document.writeln("The absolute value of" + " " + value + " " + "is:" + " " + absValue)
+
+// Question 4 
+
+// let dice1 = Math.floor( Math.random() * 7)
+// let dice2 = Math.floor( Math.random() * 6 + 1)
+
+// document.writeln("Random Dice Value:" + " " + dice1 + "<br>")
+// document.writeln("Random Dice Value:" + " " + dice2 + "<br>")
+
+// Question 5
+
+// let toss = Math.random() * 2
+
+//  let result;
+
+//  if (toss < 1) {
+//    result = "Heads"
+//  } else {
+//    result = "Tails"
+//  }
+  
+// document.writeln("Random Coin Value: " + result)
+
+// Question 6
+
+// let random = Math.floor( Math.random() * 100)
+
+// document.writeln("Random number between 1 to 100:" + " " + random)
+
+// Question 7
+
+// var userWeight = prompt("Enter your weight here in kilograms.")
+// var actualWeight = parseFloat(userWeight)
+
+// document.writeln("The weight of user is" + " " + actualWeight + " " + "kilograms.")
+
+// Question 8
+
+// let secretNumber = Math.floor(Math.random() * 11) 
+
+// console.log(secretNumber)
+
+// let userGuess = Number(prompt("Guess a number between 1 and 10:"))
+
+// if (userGuess === secretNumber) {
+//   alert(" Congratulations! You guessed the secret number: " + secretNumber)
+// } 
+//else {
+//   alert(" Sorry, You guess the wrong number.")
+// }
+
+
